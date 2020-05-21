ff8958f70631f0fcb9f04a72928a4679ec400a1d
Orhan ARIKAN <arikanorh@gmail.com>
2020-05-22 01:24:39 +0300

    Function'Ları düzenler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -35,34 +35,51 @@ export const updateSessionStats = functions.region('europe-west3').firestore.doc
 export const updateLastGameOfNewSession = functions.region('europe-west3').firestore.document('users/{userid}/sessions/{sessionid}').onCreate(
     async (snapshot, context) => {
         const uid = context.params.userid;
-  
+        const sid = context.params.sessionid;
+
         const data = snapshot.data();
-        if(data){
+        if (data) {
 
             const started = data.started;
-            const charid=data.char.id;
-            const role = data.role;            
-            
+            const charid = data.char.id;
+            const role = data.role;
+
             const previousSessionResponse = await app.firestore().collection('users/' + uid + "/sessions").
                 where('started', '<', started).
-                where('char.id','==',charid).
-                where('role','==',role).
-                orderBy('started','desc').
+                where('char.id', '==', charid).
+                where('role', '==', role).
+                orderBy('started', 'desc').
                 limit(1).get();
-            
+
+            if (previousSessionResponse.empty) {
+                console.info({
+                    message:"No previous session log found",
+                    sessionid:sid
+                    });
+                return;
+            }
+
+ 
             const previousSessionId = previousSessionResponse.docs[0].id
-            
-            const response = await app.firestore().collection('users/' + uid + "/games").where('sessionid', '==', previousSessionId).orderBy('timestamp','desc').limit(1).get();
-            
-            
-            
+
+            const response = await app.firestore().collection('users/' + uid + "/games").where('sessionid', '==', previousSessionId).orderBy('timestamp', 'desc').limit(1).get();
+
+
+
             const docs = response.docs;
-            
+
+            if(docs.length===0){
+                console.info({
+                    message:"No game entry found for previous session",
+                    previousSessionId
+                    });
+                return;
+            }
             const doc = docs[0];
-            
-            return snapshot.ref.set({last_game:doc.data()},{merge:true})
+
+            return snapshot.ref.set({ last_game: doc.data() }, { merge: true })
         }
         return;
-            
+
     }
 )
